6c5bf928dfa5e584896a0e7d0205ff4bb561e2fd
jraihan-me <jahirraihanmsd@gmail.com>
2022-06-18 21:41:12 +0600

    section 01 class 11 : other mouse event

--- section 0/app.js
+++ section 0/app.js
@@ -1,17 +1,27 @@
 const app = Vue.createApp({
     data() {
         return {
-            show: false,
-            btnText: 'show',
-            title: 'The Holy Qur\'an',
-            owner: 'ALLAH Almighty',
-            messenger: 'Prophet Muhammad (PBUH)'
+            // show: false,
+            // btnText: 'show',
+            // title: 'The Holy Qur\'an',
+            // owner: 'ALLAH Almighty',
+            // messenger: 'Prophet Muhammad (PBUH)',
+            x: 0,
+            y: 0
         }
     },
     methods: {
-        toggleShow() {
-            this.show = !this.show;
-            this.btnText = this.show ? "hide" : "show";
+        // toggleShow() {
+        //     this.show = !this.show;
+        //     this.btnText = this.show ? "hide" : "show";
+        // },
+        handleMouseEvent(e, param) {
+            console.log(e)
+            console.log(param)
+        },
+        handlemousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     },
 })
