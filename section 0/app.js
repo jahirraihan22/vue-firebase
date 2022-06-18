80d96f40c32963c329863ccc4f70761ed21df331
jraihan-me <jahirraihanmsd@gmail.com>
2022-06-18 22:28:57 +0600

    section 01 class 15 :  add to favorite challenge complete

--- section 0/app.js
+++ section 0/app.js
@@ -2,11 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             vehicles: [
-                { type: "bike", model: "R 15", manufanturer: "Yamaha", image: "assets/1.png", isFav: true },
-                { type: "car", model: "Premio", manufanturer: "Toyota", image: "assets/2.jpg", isFav: true },
-                { type: "bike", model: "GSX", manufanturer: "Suzuki", image: "assets/3.png", isFav: false },
-                { type: "bike", model: "Exmotion", manufanturer: "Honda", image: "assets/4.jpg", isFav: true },
-                { type: "car", model: "Maruti", manufanturer: "MAruti Suzuki", image: "assets/4.jpg", isFav: false },
+                { id: 0, type: "bike", model: "R 15", manufanturer: "Yamaha", image: "assets/1.png", isFav: true },
+                { id: 1, type: "car", model: "Premio", manufanturer: "Toyota", image: "assets/2.jpg", isFav: true },
+                { id: 2, type: "bike", model: "GSX", manufanturer: "Suzuki", image: "assets/3.png", isFav: false },
+                { id: 3, type: "bike", model: "Exmotion", manufanturer: "Honda", image: "assets/4.jpg", isFav: true },
+                { id: 4, type: "car", model: "Maruti", manufanturer: "MAruti Suzuki", image: "assets/4.jpg", isFav: false },
             ],
             x: 0,
             y: 0,
@@ -14,10 +14,9 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        // toggleShow() {
-        //     this.show = !this.show;
-        //     this.btnText = this.show ? "hide" : "show";
-        // },
+        addToFav(index) {
+            this.vehicles[index].isFav = !this.vehicles[index].isFav;
+        },
         handleMouseEvent(e, param) {
             console.log(e)
             console.log(param)
