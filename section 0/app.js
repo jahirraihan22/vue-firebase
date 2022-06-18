d753efa2c5b9a7441a2aa1b9b6bd118e02c29a28
jraihan-me <jahirraihanmsd@gmail.com>
2022-06-18 22:14:48 +0600

    section 01 class 14 :  class binding

--- section 0/app.js
+++ section 0/app.js
@@ -2,10 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             vehicles: [
-                { type: "bike", model: "R 15", manufanturer: "Yamaha", image: "assets/1.png" },
-                { type: "car", model: "Premio", manufanturer: "Toyota", image: "assets/2.jpg" },
-                { type: "bike", model: "GSX", manufanturer: "Suzuki", image: "assets/3.png" },
-                { type: "bike", model: "Exmotion", manufanturer: "Honda", image: "assets/4.jpg" },
+                { type: "bike", model: "R 15", manufanturer: "Yamaha", image: "assets/1.png", isFav: true },
+                { type: "car", model: "Premio", manufanturer: "Toyota", image: "assets/2.jpg", isFav: true },
+                { type: "bike", model: "GSX", manufanturer: "Suzuki", image: "assets/3.png", isFav: false },
+                { type: "bike", model: "Exmotion", manufanturer: "Honda", image: "assets/4.jpg", isFav: true },
+                { type: "car", model: "Maruti", manufanturer: "MAruti Suzuki", image: "assets/4.jpg", isFav: false },
             ],
             x: 0,
             y: 0,
