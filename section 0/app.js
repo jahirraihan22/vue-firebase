f63673d68f4c654e5501de41970ab048974b59d1
jraihan-me <jahirraihanmsd@gmail.com>
2022-06-18 22:00:27 +0600

    section 01 class 13 :  attribute binding

--- section 0/app.js
+++ section 0/app.js
@@ -2,14 +2,14 @@ const app = Vue.createApp({
     data() {
         return {
             vehicles: [
-                { type: "bike", model: "R 15", manufanturer: "Yamaha" },
-                { type: "car", model: "Premio", manufanturer: "Toyota" },
-                { type: "bike", model: "GSX", manufanturer: "Suzuki" },
-                { type: "car", model: "A 5", manufanturer: "Audi" },
-                { type: "bike", model: "Exmotion", manufanturer: "Honda" },
+                { type: "bike", model: "R 15", manufanturer: "Yamaha", image: "assets/1.png" },
+                { type: "car", model: "Premio", manufanturer: "Toyota", image: "assets/2.jpg" },
+                { type: "bike", model: "GSX", manufanturer: "Suzuki", image: "assets/3.png" },
+                { type: "bike", model: "Exmotion", manufanturer: "Honda", image: "assets/4.jpg" },
             ],
             x: 0,
-            y: 0
+            y: 0,
+            url: "https://johirraihan.com"
         }
     },
     methods: {
